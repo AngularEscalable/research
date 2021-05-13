2c4556ae1e99849db5fd065f5f318f9509314a98
Alberto Basalo <albertobasalo@hotmail.com>
2021-05-13 12:41:53 +0200

    feat: generate home page

--- apps/www/src/app/core/core-routing.module.ts
+++ apps/www/src/app/core/core-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('@rsrch/home').then((module) => module.HomeModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CoreRoutingModule { }
+export class CoreRoutingModule {}
